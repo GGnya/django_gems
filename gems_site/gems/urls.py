4fdb6dad7a36e9a12b9df2b32e4470092eac8f10
GGnya <jenia.barykin@gmail.com>
2023-02-03 06:59:25 +0300

    добавил пути

--- gems_site/gems/urls.py
+++ gems_site/gems/urls.py
@@ -1,11 +1,16 @@
 from django.urls import path
 
 # from gems.views import logout_user, LoginUser, RegisterUser
-from gems.views import GemsHome, CreateGem
+from gems.views import GemsHome, CreateGem, RegisterUser, LoginUser, Basket, Profile
 
 urlpatterns = [
     path('', GemsHome.as_view(), name='home'),
     path('create_gem/', CreateGem.as_view(), name='create_gem'),
+    path('register/', RegisterUser.as_view(), name='register'),
+    path('login/', LoginUser.as_view(), name='login'),
+    path('profile/', Profile.as_view(), name='profile'),
+    path('basket/', Basket.as_view(), name='basket'),
+
     # path('login/', LoginUser.as_view(), name='login'),
     # path('logout/', logout_user, name='logout'),
     # path('register/', RegisterUser.as_view(), name='register'),
