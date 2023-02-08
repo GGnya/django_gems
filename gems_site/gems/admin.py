c472738c25e8da44a783f94b24d8522c2e13b5d6
GGnya <jenia.barykin@gmail.com>
2023-02-08 15:14:07 +0300

    Реформат

--- gems_site/gems/admin.py
+++ gems_site/gems/admin.py
@@ -14,14 +14,16 @@ class GemAdmin(admin.ModelAdmin):
     search_fields = ('title', 'description', 'type', 'owner')
     prepopulated_fields = {'slug': ("title",)}
     fields = ('id', 'title', 'slug', 'size', 'type', 'clarity', 'gem_image', 'description',
-                    'price', 'owner', 'is_available', 'date_created', 'date_updated')
+              'price', 'owner', 'is_available', 'date_created', 'date_updated')
     readonly_fields = ('id',)
     save_on_top = True
+
     def get_html_photo(self, object):
         if object.photo:
             return mark_safe(f'<img src="{object.gem_image.url}" width=50>')
 
     get_html_photo.short_description = 'Миниатюра'
+
 
 @admin.register(GemType)
 class GemTypeAdmin(admin.ModelAdmin):
@@ -31,6 +33,7 @@ class GemTypeAdmin(admin.ModelAdmin):
     fields = ('type',)
     readonly_fields = ('id',)
     save_on_top = True
+
 
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
